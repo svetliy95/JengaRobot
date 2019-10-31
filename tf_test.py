e118146313199ad4cc2597b4ceb3754c291678b6
Andrei Svetlakov <andrei.svetlakov@rwth-aachen.de>
2019-10-31 18:01:26 +0100

    Add v-rep scene with jenga.

---
+++ tf_test.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+#import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# Install TensorFlow
+
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+with tf.device('/gpu:0'):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=10)
+
+    model.evaluate(x_test, y_test, verbose=2)
