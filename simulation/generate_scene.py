00b07586b7d84349decb035751d642ec6ac1f837
Andrei Svetlakov <andrei.svetlakov@rwth-aachen.de>
2019-11-06 19:38:27 +0100

    Evolve automatic tower generation.

--- simulation/generate_scene.py
+++ simulation/generate_scene.py
@@ -1,46 +1,57 @@
 from numpy.random import normal
 
 # global constants
-block_length_mean = 7.5
-block_width_mean = 2.5
-block_height_mean = 1.5
-timestamp = 0.001
-mass_mean = 12000
+scaler = 20
+timestep = 0.01
+# empirical data
+block_length_mean = 0.0747722 * scaler
+block_length_sigma = 0.000236 * scaler
+block_width_mean = 0.02505 * scaler
+block_width_sigma = 0.000225 * scaler
+block_height_mean = 0.0146361 * scaler
+block_height_sigma = 0.00003773 * scaler
+block_mass_mean = 0.012865 * scaler**3
+block_mass_sigma = 0.00212 * scaler**3
 
 
-def generate_block(number, mass_sigma, size_sigma, pos_sigma, angle_sigma, spacing):
+
+def generate_block(number, pos_sigma, angle_sigma, spacing):
     # TODO spacing automatic calculation
 
     if number % 6 < 3:  # even level
         x = 0
         y = -block_width_mean + (number % 3) * block_width_mean
-        y += (number % 3) * spacing
+        y += (number % 3) * spacing  # add spacing between blocks
         z = number//3 * block_height_mean
-        angle_z = normal(0, angle_sigma)
+        angle_z = normal(0, angle_sigma)  # add disturbance to the angle
     else:  # odd level
         x = -block_width_mean + (number % 3) * block_width_mean
-        x += (number % 3) * spacing
+        x += (number % 3) * spacing  # add spacing between blocks
         y = 0
         z = number//3 * block_height_mean
-        angle_z = normal(90, angle_sigma)
+        angle_z = normal(90, angle_sigma)  # rotate and add disturbance
 
-    # add disturbance
-    mass = normal(mass_mean, mass_sigma)
+    # add disturbance to mass, position and sizes
+    mass = normal(block_mass_mean, block_mass_sigma)
     [x, y] = normal([x, y], [pos_sigma, pos_sigma])
-    [block_size_x, block_size_y, block_size_z] = normal([block_length_mean/2, block_width_mean/2, block_height_mean/2], [size_sigma for x in range(3)])
+    [block_size_x, block_size_y, block_size_z] = normal([block_length_mean/2, block_width_mean/2, block_height_mean/2],
+                                                        [block_length_sigma, block_width_sigma, block_height_sigma])
 
     s = f'''
-            <body name="block{number}" pos="{x} {y} {z}" euler="0 0 {angle_z}">
+            <body name="block{number}" pos="{x} {y} {z+block_height_mean/2}" euler="0 0 {angle_z}">
                 <freejoint/>
-                <geom mass="{mass}" pos="0 0 0" rgba="1 0 0 1" size="{block_size_x} {block_size_y} {block_size_z}" type="box"/>
-            </body>
-'''
+                <geom mass="{mass}" pos="0 0 0" class="block" size="{block_size_x} {block_size_y} {block_size_z}" type="box"/>
+            </body>'''
     return s
-def generate_scene(mass_sigma = 0.002, size_sigma = 0.0005, pos_sigma = 0.001, angle_sigma = 2, spacing = 0.001):
+
+
+def generate_scene(pos_sigma=0.0005,
+                   angle_sigma=0.2,
+                   spacing=block_width_sigma*3):
 
     blocks_xml = ""
     for i in range(54):
-        blocks_xml += generate_block(i, mass_sigma, size_sigma, pos_sigma, angle_sigma, spacing)
+        blocks_xml += generate_block(i, pos_sigma, angle_sigma, spacing)
 
     MODEL_XML = f"""
     <?xml version="1.0" ?>
@@ -49,8 +60,14 @@ def generate_scene(mass_sigma = 0.002, size_sigma = 0.0005, pos_sigma = 0.001, a
         <size nconmax="3200" njmax="8000"/>
     
         <statistic extent="2" meansize=".05"/>
+        
+        <default class="main">
+            <default class="block">
+                <geom rgba="0 1 0 1" condim="4"/>
+            </default>
+        </default>
     
-        <option timestep="{timestamp}" solver="Newton" integrator="Euler" iterations="30" tolerance="1e-10" jacobian="sparse" cone="pyramidal"/>
+        <option timestep="{timestep}" integrator="Euler" cone="elliptic" solver="Newton" o_solimp="0.999 0.999 0.01 0.5 2" o_solref="0 1"/>
     
         <visual>
             <rgba haze="0.15 0.25 0.35 1"/>
@@ -87,5 +104,9 @@ def generate_scene(mass_sigma = 0.002, size_sigma = 0.0005, pos_sigma = 0.001, a
     """
     return MODEL_XML
 
+
 if __name__ == "__main__":
-    print(generate_scene(0, 0, 0, 0, 0))
+    string = generate_scene()
+    print(string)
+    file = open("/home/bch_svt/.mujoco/mujoco200/model/jenga.xml", "w")
+    file.write(string)
--- simulation/jenga.py
+++ simulation/jenga.py
@@ -2,6 +2,7 @@ from mujoco_py import load_model_from_xml, MjSim, MjViewer
 import math
 import os
 from generate_scene import generate_scene
+import time
 
 MODEL_XML = """
 <?xml version="1.0" ?>
@@ -85,13 +86,17 @@ viewer.cam.azimuth = 180
 viewer._run_speed = 1
 viewer.cam.lookat[2] = -1
 viewer.cam.distance = 15
-viewer._run_speed = 0.01
+viewer._run_speed = 1
 t = 0
 while True:
     #sim.data.ctrl[0] = math.cos(t / 1000.) # * 0.01
     #sim.data.ctrl[1] = math.sin(t / 1000.) # * 0.01
     t += 1
+    start = time.time()
     sim.step()
+
     viewer.render()
+    stop = time.time()
+    print(stop - start)
     if t > 100 and os.getenv('TESTING') is not None:
         break
