b952b6ee1547329a668c1dc16ddf9aa3cd095462
Andrei Svetlakov <andrei.svetlakov@rwth-aachen.de>
2020-10-07 18:56:46 +0200

    Add some visualization code

--- simulation/utils/compute_localization_errors.py
+++ simulation/utils/compute_localization_errors.py
@@ -23,7 +23,7 @@ rc('text.latex', preamble=r'\usepackage{cmbright}')
 
 def evaluate():
     folder = '/home/bch_svt/cartpole/simulation/localization_errors_with_quaternions'
-    filename_prefix = 'localization_errors_10822*'
+    filename_prefix = 'localization_errors*'
     all_file_names = os.listdir(folder)
     pattern = re.compile(filename_prefix)
 
@@ -112,44 +112,75 @@ def evaluate():
 
     # remove outliers
     x_errors, idxs = remove_outliers(x_errors)
-    heights = heights[idxs]
-    y_errors = y_errors[idxs]
-    z_errors = z_errors[idxs]
+    # heights = heights[idxs]
+    # y_errors = y_errors[idxs]
+    # z_errors = z_errors[idxs]
     y_errors, idxs = remove_outliers(y_errors)
-    heights = heights[idxs]
-    x_errors = x_errors[idxs]
-    z_errors = z_errors[idxs]
+    # heights = heights[idxs]
+    # x_errors = x_errors[idxs]
+    # z_errors = z_errors[idxs]
     z_errors, idxs = remove_outliers(z_errors)
-    heights = heights[idxs]
-    y_errors = y_errors[idxs]
-    x_errors = x_errors[idxs]
-    # a, idxs = remove_outliers(a)
     # heights = heights[idxs]
-    # b, idxs = remove_outliers(b)
+    # y_errors = y_errors[idxs]
+    # x_errors = x_errors[idxs]
+    a, idxs = remove_outliers(a)
     # heights = heights[idxs]
-    # c, idxs = remove_outliers(c)
+    b, idxs = remove_outliers(b)
     # heights = heights[idxs]
-    # d, idxs = remove_outliers(d)
+    c, idxs = remove_outliers(c)
     # heights = heights[idxs]
-    # x_rot_errors, idxs = np.degrees(remove_outliers(x_rot_errors))
+    d, idxs = remove_outliers(d)
     # heights = heights[idxs]
-    # y_rot_errors, idxs = np.degrees(remove_outliers(y_rot_errors))
+    x_rot_errors, idxs = remove_outliers(x_rot_errors)
+    x_rot_errors = np.degrees(x_rot_errors)
     # heights = heights[idxs]
-    # z_rot_errors, idxs = np.degrees(remove_outliers(z_rot_errors))
+    y_rot_errors, idxs = remove_outliers(y_rot_errors)
+    y_rot_errors = np.degrees(y_rot_errors)
+    # heights = heights[idxs]
+    z_rot_errors, idxs = remove_outliers(z_rot_errors)
+    z_rot_errors = np.degrees(z_rot_errors)
     # heights = heights[idxs]
 
-    for e, h in zip(x_errors, heights):
-        data_dict2.append({'axis': 'x', 'value': e, 'lvl': calculate_lvl_from_height(h)})
-    for e, h in zip(y_errors, heights):
-        data_dict2.append({'axis': 'y', 'value': e, 'lvl': calculate_lvl_from_height(h)})
-    for e, h in zip(z_errors, heights):
-        data_dict2.append({'axis': 'z', 'value': e, 'lvl': calculate_lvl_from_height(h)})
-    # for e, h in zip(x_rot_errors, heights):
-    #     data_dict2.append({'axis': 'x', 'value': e, 'height': h})
-    # for e, h in zip(y_rot_errors, heights):
-    #     data_dict2.append({'axis': 'y', 'value': e, 'height': h})
-    # for e, h in zip(z_rot_errors, heights):
-    #     data_dict2.append({'axis': 'z', 'value': e, 'height': h})
+
+    print(f"x_error mean: {np.mean(np.abs(x_errors)):.2f}")
+    print(f"y_error mean: {np.mean(np.abs(y_errors)):.2f}")
+    print(f"z_error mean: {np.mean(np.abs(z_errors)):.2f}")
+    print(f"x_rot_error mean: {np.mean(np.abs(x_rot_errors)):.2f}")
+    print(f"y_rot_error mean: {np.mean(np.abs(y_rot_errors)):.2f}")
+    print(f"z_rot_error mean: {np.mean(np.abs(z_rot_errors)):.2f}")
+    print(f"x_error 95% quantile: {np.quantile(np.abs(x_errors), 0.95):.2f}")
+    print(f"y_error 95% quantile: {np.quantile(np.abs(y_errors), 0.95):.2f}")
+    print(f"z_error 95% quantile: {np.quantile(np.abs(z_errors), 0.95):.2f}")
+    print(f"x_rot_error 95% quantile: {np.quantile(np.abs(x_rot_errors), 0.95):.2f}")
+    print(f"y_rot_error 95% quantile: {np.quantile(np.abs(y_rot_errors), 0.95):.2f}")
+    print(f"z_rot_error 95% quantile: {np.quantile(np.abs(z_rot_errors), 0.95):.2f}")
+    print(f"x_error 99% quantile: {np.quantile(np.abs(x_errors), 0.99):.2f}")
+    print(f"y_error 99% quantile: {np.quantile(np.abs(y_errors), 0.99):.2f}")
+    print(f"z_error 99% quantile: {np.quantile(np.abs(z_errors), 0.99):.2f}")
+    print(f"x_rot_error 99% quantile: {np.quantile(np.abs(x_rot_errors), 0.99):.2f}")
+    print(f"y_rot_error 99% quantile: {np.quantile(np.abs(y_rot_errors), 0.99):.2f}")
+    print(f"z_rot_error 99% quantile: {np.quantile(np.abs(z_rot_errors), 0.99):.2f}")
+    print(f"x_error mean: {np.mean(x_errors):.2f}")
+    print(f"y_error mean: {np.mean(y_errors):.2f}")
+    print(f"z_error mean: {np.mean(z_errors):.2f}")
+    print(f"x_error std: {np.std(x_errors):.2f}")
+    print(f"y_error std: {np.std(y_errors):.2f}")
+    print(f"z_error std: {np.std(z_errors):.2f}")
+
+    # exit()
+
+    # for e, h in zip(x_errors, heights):
+    #     data_dict2.append({'axis': 'x', 'value': e, 'lvl': calculate_lvl_from_height(h)})
+    # for e, h in zip(y_errors, heights):
+    #     data_dict2.append({'axis': 'y', 'value': e, 'lvl': calculate_lvl_from_height(h)})
+    # for e, h in zip(z_errors, heights):
+    #     data_dict2.append({'axis': 'z', 'value': e, 'lvl': calculate_lvl_from_height(h)})
+    for e, h in zip(x_rot_errors, heights):
+        data_dict2.append({'axis': 'x', 'value': e, 'height': h})
+    for e, h in zip(y_rot_errors, heights):
+        data_dict2.append({'axis': 'y', 'value': e, 'height': h})
+    for e, h in zip(z_rot_errors, heights):
+        data_dict2.append({'axis': 'z', 'value': e, 'height': h})
     # for e, h in zip(a, heights):
     #     data_dict2.append({'axis': 'q_a', 'value': e, 'height': h})
     # for e, h in zip(b, heights):
@@ -172,19 +203,19 @@ def evaluate():
     # exit()
 
 
-    fig_dims = (7, 2)
-    fig, ax = plt.subplots(figsize=fig_dims)
-    df = pd.DataFrame(data_dict2)
-
-    print('Hi!')
-    ax = sns.stripplot(x='lvl', y='value', hue='axis', ax=ax, data=df, dodge=True, size=3)
-    # ax = sns.scatterplot(x='height', y='value', hue='axis', ax=ax, data=df)
-    ax.set(ylabel='Fehler in mm', xlabel='Ebene')
-    plt.legend(title='Achse')
-    plt.tight_layout()
-    plt.show()
-
-    exit()
+    # fig_dims = (7, 2)
+    # fig, ax = plt.subplots(figsize=fig_dims)
+    # df = pd.DataFrame(data_dict2)
+    #
+    # print('Hi!')
+    # ax = sns.stripplot(x='lvl', y='value', hue='axis', ax=ax, data=df, dodge=True, size=3)
+    # # ax = sns.scatterplot(x='height', y='value', hue='axis', ax=ax, data=df)
+    # ax.set(ylabel='Fehler in mm', xlabel='Ebene')
+    # plt.legend(title='Achse')
+    # plt.tight_layout()
+    # plt.show()
+    #
+    # exit()
 
 
 
@@ -216,10 +247,46 @@ def evaluate():
     # sns.scatterplot(x_errors, b)
 
     # compute parameters for all features
-    # dist = stats.norm
-    # dist = stats.cauchy
-    # dist = stats.hypsecant
-    dist = stats.norminvgauss
+    dist_norm = stats.norm
+    dist_cauchy = stats.cauchy
+    dist_hypsecant = stats.hypsecant
+    dist_norminv = stats.norminvgauss
+
+
+    # distributions comparison
+    params_norm_x = dist_norm.fit(x_errors)
+    params_cauchy_x = dist_cauchy.fit(x_errors)
+    params_hypsecant_x = dist_hypsecant.fit(x_errors)
+    params_x = dist_norminv.fit(x_errors)
+    loglh_norm_x = dist_norm.logpdf(x_errors, params_norm_x[0], params_norm_x[1]).sum()
+    loglh_cauchy_x = dist_cauchy.logpdf(x_errors, params_cauchy_x[0], params_cauchy_x[1]).sum()
+    loglh_hypsecant_x = dist_hypsecant.logpdf(x_errors, params_hypsecant_x[0], params_hypsecant_x[1]).sum()
+    loglh_norminvgauss_x = dist_norminv.logpdf(x_errors, params_x[0], params_x[1], params_x[2], params_x[3]).sum()
+    print(f"Loglhs: {loglh_norm_x}, {loglh_cauchy_x}, {loglh_hypsecant_x}, {loglh_norminvgauss_x}")
+
+    params_norm_x = dist_norm.fit(y_errors)
+    params_cauchy_x = dist_cauchy.fit(y_errors)
+    params_hypsecant_x = dist_hypsecant.fit(y_errors)
+    params_x = dist_norminv.fit(y_errors)
+    loglh_norm_x = dist_norm.logpdf(y_errors, params_norm_x[0], params_norm_x[1]).sum()
+    loglh_cauchy_x = dist_cauchy.logpdf(y_errors, params_cauchy_x[0], params_cauchy_x[1]).sum()
+    loglh_hypsecant_x = dist_hypsecant.logpdf(y_errors, params_hypsecant_x[0], params_hypsecant_x[1]).sum()
+    loglh_norminvgauss_x = dist_norminv.logpdf(y_errors, params_x[0], params_x[1], params_x[2], params_x[3]).sum()
+    print(f"Loglhs: {loglh_norm_x}, {loglh_cauchy_x}, {loglh_hypsecant_x}, {loglh_norminvgauss_x}")
+
+    params_norm_x = dist_norm.fit(z_errors)
+    params_cauchy_x = dist_cauchy.fit(z_errors)
+    params_hypsecant_x = dist_hypsecant.fit(z_errors)
+    params_x = dist_norminv.fit(z_errors)
+    loglh_norm_x = dist_norm.logpdf(z_errors, params_norm_x[0], params_norm_x[1]).sum()
+    loglh_cauchy_x = dist_cauchy.logpdf(z_errors, params_cauchy_x[0], params_cauchy_x[1]).sum()
+    loglh_hypsecant_x = dist_hypsecant.logpdf(z_errors, params_hypsecant_x[0], params_hypsecant_x[1]).sum()
+    loglh_norminvgauss_x = dist_norminv.logpdf(z_errors, params_x[0], params_x[1], params_x[2], params_x[3]).sum()
+    print(f"Loglhs: {loglh_norm_x}, {loglh_cauchy_x}, {loglh_hypsecant_x}, {loglh_norminvgauss_x}")
+
+
+
+
     print(f"Start fit")
     # params_x = dist.fit(x_errors)
     # params_y = dist.fit(y_errors)
